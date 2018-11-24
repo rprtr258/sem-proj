da51ba5667a324a670cbb146df46d978a8457698
Poletansky Viktor <poletansky@gmail.com>
2018-11-24 19:40:38 +0300

    Fix un

--- gun.cpp
+++ gun.cpp
@@ -1,4 +1,7 @@
 #include "gun.h"
 
-Gun::~Gun() {
+Bullet* Gun::shoot(Observer *view, QVector2D mouseCoord, QVector2D playerCoord, Map &map) {
+    QQuickItem *bulletItem = view->createBullet((qint32)playerCoord.x(), (qint32)playerCoord.y());
+    Bullet *bullet = new Bullet(bulletItem, mouseCoord - playerCoord, map);
+    return bullet;
 }
--- gun.h
+++ gun.h
@@ -6,12 +6,7 @@
 
 class Gun : public Weapon {
     public:
-        virtual ~Gun();
-        virtual Bullet* shoot(Observer *view, QVector2D mouseCoord, QVector2D playerCoord, Map &map) {
-            QQuickItem *bulletItem = view->createBullet((qint32)playerCoord.x(), (qint32)playerCoord.y());
-            Bullet *bullet = new Bullet(bulletItem, mouseCoord - playerCoord, map);
-            return bullet;
-        }
-    };
+        Bullet* shoot(Observer *view, QVector2D mouseCoord, QVector2D playerCoord, Map &map) override;
+};
 
 #endif // GUN_H
